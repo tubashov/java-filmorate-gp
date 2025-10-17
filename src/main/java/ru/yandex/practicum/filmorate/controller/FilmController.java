8f532cd90ea61174b6f3ba234410807bfce02419
gennadiyniki <nikitkogs@gmail.com>
2025-10-17 16:41:26 +0300

    add-director

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -9,16 +9,6 @@ import ru.yandex.practicum.filmorate.service.FilmService;
 
 import java.util.List;
 
-/**
- * {@code GET /films} - Получение фильмов<br/>
- * {@code GET /films/{id}} - Получение фильма<br/>
- * {@code GET /films/popular} - Получение популярных фильмов<br/>
- * {@code POST /films} - Создание фильма<br/>
- * {@code PUT /films} - Редактирование фильма<br/>
- * {@code PUT /films/{id}/like/{userId}} - Лайк фильму<br/>
- * {@code DELETE /films/{id}/like/{userId}} - Удаление лайка<br/>
- */
-
 @RestController
 @RequestMapping("/films")
 @Slf4j
