75f5fd41012ae5e3a36d0ceb4b60c7404d14a1ab
tubashov <tubashov@yandex.ru>
2025-10-21 17:32:49 +0300

    Checkstyle.

--- src/main/java/ru/yandex/practicum/filmorate/storage/feed/FeedStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/feed/FeedStorage.java
@@ -4,6 +4,8 @@ import ru.yandex.practicum.filmorate.model.Event;
 import java.util.List;
 
 public interface FeedStorage {
+
     void addEvent(Event event);
+
     List<Event> getUserFeed(Long userId);
 }
